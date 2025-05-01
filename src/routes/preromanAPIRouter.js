72e95aa341363cb42d188a7f922339ff174fbff9
Sandra Fernandez <sandramedz@gmail.com>
2025-05-01 12:22:56 +0200

    se pueden ver los sitios sin autenticarse

--- src/routes/preromanAPIRouter.js
+++ src/routes/preromanAPIRouter.js
@@ -6,16 +6,16 @@ import { requireAdmin } from "../middleware/requireAdmin.js";
 const router = Router();
 
 // Conseguir todo el arte prerrománico
-router.get("/", authenticateToken, preromanAPIController.getAll);
+router.get("/", preromanAPIController.getAll);
 
 // Crear un nuevo lugar de arte prerrománico
 router.post("/", authenticateToken, requireAdmin, preromanAPIController.create);
 
 // Conseguir un sitio de arte prerrománico por su id
-router.get("/:id", authenticateToken, preromanAPIController.getByID);
+router.get("/:id", preromanAPIController.getByID);
 
 // Conseguir todo el arte prerrománico de un concejo
-router.get("/by-council/:council_id", authenticateToken, preromanAPIController.getByCouncil);
+router.get("/by-council/:council_id", preromanAPIController.getByCouncil);
 
 // Editar un sitio de arte prerrománico con su id
 router.put("/:id", authenticateToken, requireAdmin, preromanAPIController.edit);
