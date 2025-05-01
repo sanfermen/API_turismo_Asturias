e744aaa03dd8b63501f7e6fbb150255606664cc9
Sandra Fernandez <sandramedz@gmail.com>
2025-05-01 11:00:47 +0200

    modelos favorito y visitados

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 import { DataTypes } from "sequelize";
 import connection from "../config/sequelize.js";
+import Favourite from "./favourite.js";
+import Visited from "./visited.js";
 
 const User = connection.define("user", {
 	user_id: {
@@ -27,5 +29,11 @@ const User = connection.define("user", {
 		defaultValue: "user"
 	}
 })
+
+User.hasMany(Favourite, { foreignKey: 'user_id', onDelete: 'CASCADE' });
+Favourite.belongsTo(User, { foreignKey: 'user_id' });
+
+User.hasMany(Visited, { foreignKey: 'user_id', onDelete: 'CASCADE' });
+Visited.belongsTo(User, { foreignKey: 'user_id' });
 
 export default User;
