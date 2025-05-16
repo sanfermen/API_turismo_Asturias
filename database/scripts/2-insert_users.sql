e202e92eb538a6e9bcaf097eed84ee5db39b344c
Sandra Fernandez <sandramedz@gmail.com>
2025-05-16 07:45:03 +0200

    Deja de trackear archivo de usuarios

--- database/scripts/2-insert_users.sql
+++
@@ -1,7 +0,0 @@
-USE `asturias`;
-
-INSERT INTO `asturias`.`user` (`name`, `email`, `password`, `role`) VALUES
-('Sandra', 'sandra@ejemplo.com', '$2b$10$.ct0XPGij/a/wfrqzeYyu.TUH3WQ9onuG4J127zflvDWn1YUZ7ZIS', 'admin'),
-('Juan', 'juan@ejemplo.com', '$2b$10$Qx4kjk/aFa3UhFJADo74MeduhmcAmeop.ECbHilIcaJhP3EuRV.0.', 'user'),
-('Lucía', 'lucia@ejemplo.com', '$2b$10$CaakfaqD9i0uOQ2TQXadA.xsrYnmsX/Krdwj57NZeoSDvaX022Vl.', 'user'),
-('Miguel', 'miguel@ejemplo.com', '$2b$10$j6GI4E.tUVyDZX8aNItPxex/0kiv6lQ5rqd3L3hdkBb6S6uveCSs.', 'user');
